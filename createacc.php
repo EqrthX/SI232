58e0080c7d6cf50672b1228068cbd01e45715fa4
d0hyuneiei <167629910+d0hyuneiei@users.noreply.github.com>
2024-04-21 01:31:13 +0700

    Add files via upload

---
+++ createacc.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <title>createaccount</title>
+    <meta charset="UTF-8">   
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">           
+         
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />   
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="create.css">
+   </head> 
+
+
+<body>
+<h1> Createa&nbsp;Account</h2>
+<img src="dogregis.jpg" alt="">
+<form action="checkRegister.php" method="post">
+
+<label for="" class="FirstName">FirstName</label>
+<input type="text"class="boxfname" name="" id="">
+
+<label for=""class="LastName">LastName</label>
+<input type="text"class="boxlname" name="" id="">
+
+<label for=""class="Email">Email</label>
+<input type="text"class="boxemail" name="" id="">
+
+<label for=""class="Career">Career</label>
+<input type="text"class="boxcareer" name="" id="">
+
+<label for=""class="Country">Country</label>
+<input type="text"class="boxcountry" name="" id="">
+
+<label for=""class="Birthdate">Birthdate</label>
+<input type="date"class="boxbd" name="" id="">
+
+<label for="password"class="Password">Password</label>
+<input type="password"class="boxpass" id="password" name="password"  required><br>
+
+ <label for="password"class="Confpassword">Confpassword</label>
+<input type="password"class="boxconfpass" id="password" name="confpassword"  required><br>
+
+
+<input type="submit"class="botton" id="password" name="confpassword"  required><br>
+   
+  
+
+</form> 
+</body>
+</html>
