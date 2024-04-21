60cfa7327e7f5b6d02850c3f15aa9e772daa25e9
EqrthX <nontprawitchkung@gmail.com>
2024-04-21 20:42:09 +0700

    flagend

--- home.php
+++ home.php
@@ -6,31 +6,26 @@
     <title>Your Website Title</title>
     <!-- นี่คือส่วนสำหรับเชื่อมโยงไฟล์ CSS ถ้ามี -->
     <link rel="stylesheet" href="home.css">
+    
 </head>
 <body>
 
 <!-- เริ่มต้นโค้ด HTML ของคุณที่นี่ -->
-<div class="container">
-  <div class="rectangle5"></div>
-  <div class="web1"></div>
-  <p class="text1">ทุกชีวิตบนโลก ล้วนมีค่าเสมอ</p>
-  <p class="text2">ให้เราได้ช่วยคุณ</p>
-  <div class="ellipse1"></div>
-  <a class="link1" href="#">เข้าสู่เว็บไซต์</a>
-  <div class="rectangle6"></div>
-  <div class="vector"></div>
-  <div class="searching-bar"></div>
-  <p class="text3">หน้าแรก</p>
-  <p class="text4">คัดกรอง</p>
-  <p class="text5">ช่วยเหลือ</p>
-  <p class="text6">โพสต์</p>
-  <div class="flag1"></div>
-  <div class="flag2"></div>
-  <p class="lang1">TH</p>
-  <p class="lang2">EN</p>
-  <div class="profile"></div>
-</div>
-<!-- สิ้นสุดโค้ด HTML ของคุณ -->
+
+  <?php require"navbar.php"; ?>
+
+  <h1 style="text-align: center;">ทุกชีวิตบนโลก ล้วนมีค่าเสมอ</h1>
+
+  <div class="box-pic">
+
+    <img src="pic5.png" alt="">
+
+  </div>
+  
+  <h3 style="text-align: center;">ให้เราได้ช่วยคุณ</h3>
+  
+  <button><a href="">เข้าสู่เว็บไซต์</a></button>
+
 
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,17 @@
+<div class="navbar">
+
+    <ul>
+
+      <li><img src="icon.png" alt="" id="icon1"></li>
+      <li><input type="text"></li>
+      <li><a href="" id="nav-home">หน้าแรก</a></li>
+      <li><a href="" id="nav-fillter">คัดกรอก</a></li>
+      <li><a href="" id="nav-help">ช่วยเหลือ</a></li>
+      <li><a href="" id="nav-post">โพสต์</a></li>
+      <li><a href=""><img src="Profile.png" alt="" width="49" height="49" class="icon-people"></a></li>    
+      <li><img src="flagthai.png" alt="" width="51" height="51" class="flagT"></li>
+      <li><img src="flageng.png" alt="" width="51" height="51" class="flagE"></li>
+    
+    </ul>
+
+  </div>
