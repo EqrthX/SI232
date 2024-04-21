f88d478cfc825914984638d620200185abf7e0d6
Napatnamo012 <166981824+Napatnamo012@users.noreply.github.com>
2024-04-21 19:11:37 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+    <!-- นี่คือส่วนสำหรับเชื่อมโยงไฟล์ CSS ถ้ามี -->
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+<!-- เริ่มต้นโค้ด HTML ของคุณที่นี่ -->
+<div class="container">
+  <div class="rectangle5"></div>
+  <div class="web1"></div>
+  <p class="text1">ทุกชีวิตบนโลก ล้วนมีค่าเสมอ</p>
+  <p class="text2">ให้เราได้ช่วยคุณ</p>
+  <div class="ellipse1"></div>
+  <a class="link1" href="#">เข้าสู่เว็บไซต์</a>
+  <div class="rectangle6"></div>
+  <div class="vector"></div>
+  <div class="searching-bar"></div>
+  <p class="text3">หน้าแรก</p>
+  <p class="text4">คัดกรอง</p>
+  <p class="text5">ช่วยเหลือ</p>
+  <p class="text6">โพสต์</p>
+  <div class="flag1"></div>
+  <div class="flag2"></div>
+  <p class="lang1">TH</p>
+  <p class="lang2">EN</p>
+  <div class="profile"></div>
+</div>
+<!-- สิ้นสุดโค้ด HTML ของคุณ -->
+
+</body>
+</html>
