a89eb7ed745d652fe92961b029653d53b630f568
EqrthX <nontprawitchkung@gmail.com>
2024-04-29 22:18:57 +0700

    แก้ให้ละ

--- login.php
+++ login.php
@@ -1,20 +1,47 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <link rel = "stylesheet" href = "LOGIN.css">
-    </head>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="LOGIN.css">
+</head>
 <body>
+  
+  <div class="container">
 
-<h2>Sign In</h2>
+    <div class="box-login">
 
-<form action="checkLogin.php" method="post">
-  <label for="Email">Email:</label><br>
-  <input type="email" id="Email" name="Email" required><br>
-  <label for="Password">Password:</label><br>
-  <input type="password" id="password" name="password" required><br><br>
-  <input class ="box"type="submit" value="Submit"> <input type="reset" value="Reset">
-</form> 
+      <div class="login">
 
+        <h2>Sign In</h2>
+
+        <form action="checkLogin.php" method="post">
+
+          <label for="Email">Email:</label>
+          <input type="email" id="Email" name="Email" placeholder="xxxxx@gmail.com" required><br>
+          <label for="Password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="XXXXXXXX" required><br>
+          
+          <input class="box" type="submit" value="Submit">
+          <a href="#" id="fp">Forget Password?</a>
+          
+        </form>
+
+        <p>Don't have an account? <a href="createacc.php">Create</a></p>
+
+      </div>
+      
+
+    </div>
+
+    <div class="box-img">
+
+      <img src="cat.png" alt="">
+
+    </div>
+    
+  </div>
 
 </body>
 </html>
