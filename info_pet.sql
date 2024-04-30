dcfcf531f3d245a8dd85d5e79cba80272b6faa03
babongbong <167644276+babongbong@users.noreply.github.com>
2024-04-30 20:43:28 +0700

    Add files via upload

---
+++ info_pet.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 30, 2024 at 03:42 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `info_pet`
+--
+
+CREATE TABLE `info_pet` (
+  `id_pet` int(10) NOT NULL,
+  `gender_pet` varchar(10) NOT NULL,
+  `color_pet` varchar(10) NOT NULL,
+  `type_pet` varchar(10) NOT NULL,
+  `vac_pet` varchar(300) NOT NULL,
+  `info_pet` text NOT NULL,
+  `name_pet` varchar(10) NOT NULL,
+  `img_pet` blob NOT NULL,
+  `gene_pet` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `info_pet`
+--
+ALTER TABLE `info_pet`
+  ADD PRIMARY KEY (`id_pet`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
